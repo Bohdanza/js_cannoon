06a25900efedf99c7d4a3c3e7787d88f10031925
Bohdanza <bohdanza87@gmail.com>
2025-01-20 02:14:47 +0200

    Guess we're doin phaser

--- main.js
+++ main.js
@@ -1,58 +1,30 @@
-//Main class. Does all the heavy lifting: main loop,
-//window resize handling etc.
-class GameInstance
+var config = 
 {
-    #tickPause=16.66666;
-
-    constructor()
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: 
     {
-        this.canvas=document.getElementById("mainCanvas");
-        this.glContext=this.canvas.getContext("webgl");
-
-        this.glContext.clearColor(0, 0, 0, 255);
- 
-        this.#onResize();
-        window.addEventListener('resize', ()=>{this.#onResize()});
-        this.mainLoop();
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-    #update()
-    {
-        
-    }
+var game = new Phaser.Game(config);
 
-    #draw()
-    {    
-        this.glContext.clear(this.glContext.COLOR_BUFFER_BIT);
-        //draw everything else
-    }
-
-    async mainLoop()
-    {
-        var dt1, dt2;
-
-        while(1)
-        {
-            await new Promise((resolve, reject) => {dt1=(new Date()).getTime(); /*console.log(dt1);*/ resolve();});
-
-            await new Promise((resolve, reject) => {this.#update(); resolve();});
-            await new Promise((resolve, reject) => {this.#draw(); resolve();});
-
-            await new Promise((resolve, reject) => {dt2=(new Date()).getTime(); resolve();});
-
-            await new Promise((resolve, reject) => {setTimeout(()=>resolve(), Math.max(0, this.#tickPause-dt2+dt1))});
-        }
-    }
-
-    //What we do if window is resized
-    #onResize()
-    {
-        this.realWindowWidth=window.innerWidth;
-        this.realWindowHeight=window.innerHeight;
-
-        this.canvas.width=this.realWindowWidth;
-        this.canvas.height=this.realWindowHeight;
-    }
+function preload ()
+{
+    this.load.image('test', 'Content\\testimage.png');
 }
 
-mainGameInstance = new GameInstance();
+function create ()
+{
+    this.add.image(100, 100, 'test').setDepth(0);
+    
+    this.add.image(40, 100, 'test').setDepth(1);
+}
+
+function update ()
+{
+}
